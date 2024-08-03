cf5678f5d4530113022f78ca122d024935a5e499
lareenmelo <lareenmmelo@gmail.com>
2024-08-03 12:21:43 -0400

    making dundermart a mac framework

---
+++ dundermart/dundermart/dundermart.h
@@ -0,0 +1,18 @@
+//
+//  dundermart.h
+//  dundermart
+//
+//  Created by Lareen Melo on 8/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dundermart.
+FOUNDATION_EXPORT double dundermartVersionNumber;
+
+//! Project version string for dundermart.
+FOUNDATION_EXPORT const unsigned char dundermartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dundermart/PublicHeader.h>
+
+
