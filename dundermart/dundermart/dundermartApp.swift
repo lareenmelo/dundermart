5672d00d8a0dd4d07a299d02404392bd734c5b2c
lareenmelo <lareenmmelo@gmail.com>
2024-08-02 22:56:14 -0400

    adding dundermart xcode project (empty dummy project)

---
+++ dundermart/dundermart/dundermartApp.swift
@@ -0,0 +1,17 @@
+//
+//  dundermartApp.swift
+//  dundermart
+//
+//  Created by Lareen Melo on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct dundermartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
